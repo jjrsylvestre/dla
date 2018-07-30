c069fbb6d3540b4a2c73f5c2d0acc6b0f61fd9d7
Jeremy Sylvestre <jeremy.sylvestre@ualberta.ca>
2018-07-29 22:21:49 -0600

    Add index; add section TOCs in assemblage; reformat Examples sections in each chapter.

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -15,10 +15,23 @@ then
 	exit 1
 fi
 
-# even though a directory is allowed as argument for --output option,
-# xsltproc gives a "I/O error : Is a directory" error
-# but using a throw-away main output file avoids the error
-# (and DELETEME.html is just blank after processing anyway)
+# Even though a directory is allowed as argument for --output option,
+# xsltproc gives a "I/O error : Is a directory" error.
+# But using a throw-away main output file avoids the error
+# (and DELETEME.html is just blank after processing anyway).
 
-xsltproc --xinclude --output build/html/DELETEME.html style.xsl src/book.xml
+xsltproc \
+  --xinclude \
+	--output build/html/DELETEME.html \
+	--stringparam numbering.projects.level 2 \
+	--stringparam html.knowl.example no \
+	style.xsl src/book.xml
 rm build/html/DELETEME.html
+
+# UNUSED STRINGPARAMS
+
+#	--stringparam numbering.theorems.level 2
+# ^^^^ Don't like this: theorems share numbering with remarks, etc., so theorem numbering won't start at 1 in each Theory section.
+
+# --stringparam toc.level 2 \
+# ^^^^ In principle I like this, but in current incarnation it makes it difficult to navigate. Wish there was a toc.level=2.5.
