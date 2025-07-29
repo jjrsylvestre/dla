8f09a223c47442d1ff87a52b0d34e9a159db49ed
Jeremy Sylvestre <jeremy.sylvestre@ualberta.ca>
2025-07-29 12:59:37 -0600

    Scripts: output image label when calculating widths

--- scripts/image-widths.sh
+++ scripts/image-widths.sh
@@ -4,5 +4,5 @@ for F in ${*}
 do
 	width=`pdfinfo ${F} | tr -s ' ' | grep "Page size" | cut -f3 -d' '`
 	width_pct=`echo "scale=1; 100*${width}/340" | bc`
-	echo "`basename ${F}`: ${width_pct}%"
+	echo "${width_pct}%  `basename ${F} .pdf`"
 done
